cccc6c044f408bd1cb38e832006cce90fe49e23f
Maxim Nikiforov <nikim@farmingdale.edu>
2025-01-30 13:08:28 -0500

    init commit

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle {
+    String brand;
+    public car(String brand,
+    public void honk() {
+        System.out.println("Honk,honk!");
+    }
+    public void displayInfo() {
+
+
+    }
+
+}
