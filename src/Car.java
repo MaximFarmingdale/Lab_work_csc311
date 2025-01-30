889b93e5417843159475efdae61aaea4718b2824
Maxim Nikiforov <nikim@farmingdale.edu>
2025-01-30 13:30:09 -0500

    init commit

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,18 @@
 public class Car extends Vehicle {
     String brand;
-    public car(String brand,
+    public Car(String color, int numberOfWheels, float engineSize, String fueltype, String brand) {
+    super(color, numberOfWheels, engineSize, fueltype);
+        this.brand = brand;
+    }
     public void honk() {
         System.out.println("Honk,honk!");
     }
     public void displayInfo() {
-
+    System.out.println("Brand: " + brand);
+    System.out.println("Engine Size: " + engineSize);
+    System.out.println("Fuel type: " + fueltype);
+    System.out.println("Color: " + color);
+    System.out.println("Number of Wheels: " + numberOfWheels);
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        new
+        Vehicle Vehicle_one = new Vehicle("blue",4, 16, "Gasoline");
+        Vehicle_one.set_color("red");
+        Vehicle_one.set_fueltype("Leaded Gasoline");
+        Vehicle_one.set_engineSize(12);
+        Vehicle_one.set_numberOfWheels(5);
+        System.out.println(Vehicle_one.getNumberOfWheels());
+        System.out.println(Vehicle_one.getColor());
+        System.out.println(Vehicle_one.getFueltype());
+        System.out.println(Vehicle_one.getEngineSize());
+        Car car_one = new Car("blue",4,12,"Gasoline","BMW");
+        car_one.honk();
+        car_one.displayInfo();
     }
 }
